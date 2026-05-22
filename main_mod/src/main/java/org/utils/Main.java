a3bc607e14f42a34033ce5eb7157c1e8a01097ff
hakuiiki <nacho.placenciap@gmail.com>
2026-05-22 11:03:31 -0400

    hol

---
+++ main_mod/src/main/java/org/utils/Main.java
@@ -0,0 +1,13 @@
+package org.utils;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        IO.println(String.format("Hello and welcome!"));
+
+        System.out.println("A borrar 0");
+    }
+}
+
